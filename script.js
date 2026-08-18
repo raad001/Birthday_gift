1d4f2876d71059ac42b5a7d1cda9b5b835a2fd07
raad001 <105879588+raad001@users.noreply.github.com>
2026-08-18 13:34:56 +0600

    Update script.js

--- script.js
+++ script.js
@@ -392,7 +392,7 @@ secretBtn.addEventListener("click",()=>{
     Thank you for being a wonderful person
     and bringing positive energy & hapiness to people around you.
 
-    Never forget how special you are ✨
+    Never forget how special you are and never stop chasing your goals, BEST OF LUCK CHAMP💪✨
     </p>
 
     `;
